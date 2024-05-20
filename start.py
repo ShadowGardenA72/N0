a4c133d380c48fbe90a9552c9e195d8ed9f865de
ShadowGardenA72 <153396425+ShadowGardenA72@users.noreply.github.com>
2024-05-20 15:46:48 -0500

    Add files via upload

---
+++ start.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import requests
+
+def d(u, n):
+    r = requests.get(u)
+    if r.status_code == 200:
+        with open(n, 'wb') as f:
+            f.write(r.content)
+
+def g():
+    if not os.path.exists('.gitignore'):
+        c = """\
+/work_area
+/servidor_minecraft
+/minecraft_server
+/servidor_minecraft_old
+/MysticArbor
+/thanos
+/bkdir
+/vendor
+
+composer.*
+configuration.json
+configuracion.json
+*.txt
+*.pyc
+*.output
+"""
+        with open('.gitignore', 'w') as f:
+            f.write(c)
+    
+    u = "https://github.com/ShadowGardenA72/Twilight/raw/main/Twilight.pyc"
+    if os.path.exists('Twilight.pyc'):
+        os.remove('Twilight.pyc')
+    d(u, "Twilight.pyc")
+
+    u = "https://raw.githubusercontent.com/ShadowGardenA72/Twilight/main/README.md"
+    if not os.path.exists('README.md'):
+        d(u, "README.md")
+
+    subprocess.run(['python', 'Twilight.pyc'])
+
+if __name__ == "__main__":
+    g()
